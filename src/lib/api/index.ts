892985039ebb5ae85727cdf013accb581c7fece1
Domey Benjamin <domeybenjamin1@gmail.com>
2023-04-23 16:39:43 +0000

    updated api

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,17 +1,18 @@
 import express, { RequestHandler, Router } from 'express';
 import { defaultTo, pick, prop } from 'ramda';
 import { ICradle } from '../dependency';
+import debug from 'debug';
 
 const API = express.Router();
 
 type IAPI_METHODS = 'get' | 'post' | 'patch' | 'put' | 'delete';
 
 const successCodes: Record<IAPI_METHODS, number> = {
-  'get': 200,
-  'post': 201,
-  'patch': 200,
-  'put': 200,
-  'delete': 204,
+  get: 200,
+  post: 201,
+  patch: 200,
+  put: 200,
+  delete: 204,
 };
 
 const getSuccessCode = (method: IAPI_METHODS) =>
@@ -24,51 +25,42 @@ interface IEndpoint {
   handler: Function;
 }
 
-
 const createHandleRequestResponse =
-  (handler: Function) =>
+  ({ handler, log }: { handler: Function; log: debug.Debugger }) =>
   ({ path, method, middlewares }: Omit<IEndpoint, 'handler'>) =>
-    API[method](
-      path,
-      ...(middlewares ?? []),
-      async (req, res, next) => {
-        try {
-          const { query, params, body } = pick(
-            ['query', 'params', 'body'],
-            req
-          );
-          const output = await handler({
-            ...query,
-            ...params,
-            ...body,
-          });
-          const successCode = getSuccessCode(method);
-          console.log(successCode)
-          return res
-            .status(successCode)
-            .json({ success: true, payload: output });
-        } catch (e) {
-          next(e);
-        }
+    API[method](path, ...(middlewares ?? []), async (req, res, next) => {
+      try {
+        const { query, params, body } = pick(['query', 'params', 'body'], req);
+        const output = await handler({
+          ...query,
+          ...params,
+          ...body,
+        });
+        const successCode = getSuccessCode(method);
+
+        log(JSON.stringify({ url: req.url, res: output, status: successCode }));
+        return res.status(successCode).json({ success: true, payload: output });
+      } catch (e) {
+        next(e);
       }
+    });
+
+export default ({ pingService, createLogger }: ICradle) =>
+  () => {
+    const log = createLogger('api');
+    const endpointInformation: Array<IEndpoint> = [
+      {
+        handler: pingService.ping,
+        method: 'get',
+        path: '/api',
+      },
+    ];
+
+    endpointInformation.map(({ handler, ...ctx }) =>
+      createHandleRequestResponse({ handler, log })(ctx)
     );
-
-export default ({
-  pingService
-}: ICradle) => () => {
-  const endpointInformation: Array<IEndpoint> = [
-    {
-      handler: pingService.ping,
-      method: 'get',
-      path: '/api'
-    }
-  ];
-
-  endpointInformation.map(({ handler, ...ctx }) =>
-    createHandleRequestResponse(handler)(ctx)
-  );
-  return { api: API, endpointInformation };
-};
+    return { api: API, endpointInformation };
+  };
 
 export type IApiType = () => {
   api: Router;
